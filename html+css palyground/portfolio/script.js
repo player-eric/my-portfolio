5950d002e194f33b28112def8f085aaccc6273c5
Shiqin Yan <digimonyan@gmail.com>
2021-05-18 23:48:42 -0400

    profile page left and right sections clickable

--- html+css palyground/portfolio/script.js
+++ html+css palyground/portfolio/script.js
@@ -1,10 +1,10 @@
-const totalItems = 2;
+const totalItems = 3;
 
-var list = $(".left-slider");
+var list = $(".left-section");
 var listItems = list.children(".item");
 list.append(listItems.get().reverse());
 
-$(".right-slider")
+$(".right-section")
 	.slick({
 		vertical: true,
 		verticalSwiping: true,
@@ -22,34 +22,68 @@ $(".right-slider")
 			nextSlide == 0 &&
 			currentSlide == totalItems - 1
 		) {
-			$(".left-slider").slick("slickGoTo", -1);
+			$(".left-section").slick("slickGoTo", -1);
 		} else if (
 			currentSlide < nextSlide &&
 			currentSlide == 0 &&
 			nextSlide == totalItems - 1
 		) {
-			$(".left-slider").slick("slickGoTo", totalItems);
+			$(".left-section").slick("slickGoTo", totalItems);
 		} else {
-			$(".left-slider").slick("slickGoTo", totalItems - 1 - nextSlide);
+			$(".left-section").slick("slickGoTo", totalItems - 1 - nextSlide);
 		}
 	})
 	.on("mousewheel", function (event) {
 		event.preventDefault();
-		if (event.deltaX > 0 || event.deltaY < 0) {
+		if (event.deltaX < 0 || event.deltaY > 0) {
 			$(this).slick("slickPrev");
-		} else if (event.deltaX < 0 || event.deltaY > 0) {
+		} else if (event.deltaX > 0 || event.deltaY < 0) {
 			$(this).slick("slickNext");
 		}
 	});
 
-$(".left-slider").slick({
-	swipe: false,
-	vertical: true,
-	arrows: false,
-	infinite: true,
-	speed: 400,
-	cssEase: "cubic-bezier(0.7, 0, 0.3, 1)",
-	initialSlide: totalItems - 1,
+$(".left-section")
+	.slick({
+		swipe: false,
+		vertical: true,
+		arrows: false,
+		infinite: true,
+		speed: 400,
+		cssEase: "cubic-bezier(0.7, 0, 0.3, 1)",
+		initialSlide: totalItems - 1,
 
-	touchThreshold: 20,
-});
+		touchThreshold: 20,
+	})
+	.on("beforeChange", function (event, slick, currentSlide, nextSlide) {
+		if (
+			currentSlide > nextSlide &&
+			nextSlide == 0 &&
+			currentSlide == totalItems - 1
+		) {
+			$(".right-section").slick("slickGoTo", -1);
+		} else if (
+			currentSlide < nextSlide &&
+			currentSlide == 0 &&
+			nextSlide == totalItems - 1
+		) {
+			$(".right-section").slick("slickGoTo", totalItems);
+		} else {
+			$(".right-section").slick("slickGoTo", totalItems - 1 - nextSlide);
+		}
+	})
+	.on("mousewheel", function (event) {
+		event.preventDefault();
+		if (event.deltaX < 0 || event.deltaY > 0) {
+			$(this).slick("slickNext");
+		} else if (event.deltaX > 0 || event.deltaY < 0) {
+			$(this).slick("slickPrev");
+		}
+	});
+
+function toSkills() {
+	$(".right-section").slick("slickGoTo", 1);
+}
+
+function toProjects() {
+	$(".right-section").slick("slickGoTo", 2);
+}
