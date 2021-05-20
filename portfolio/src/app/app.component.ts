71a2b314475a33be3da50a05ceed413c43295b37
Shiqin Yan <digimonyan@gmail.com>
2021-05-20 18:06:23 -0400

    migrated playground codes into an Angular app

---
+++ portfolio/src/app/app.component.ts
@@ -0,0 +1,146 @@
+import { Component, OnInit } from '@angular/core';
+declare var $: any;
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css','./app.slick.css']
+})
+export class AppComponent implements OnInit {
+  ngOnInit(){
+      const totalItems = 3;
+      var list = $(".left-section");
+      var listItems = list.children(".item");
+      list.append(listItems.get().reverse());
+
+      $(".right-section")
+        .slick({
+          vertical: true,
+          verticalSwiping: true,
+          arrows: false,
+          infinite: true,
+          speed: 400,
+          cssEase: "cubic-bezier(0.7, 0, 0.3, 1)",
+
+          touchThreshold: 20,
+          mobileFirst: true,
+        })
+        .on("beforeChange", function (event, slick, currentSlide, nextSlide) {
+          if (
+            currentSlide > nextSlide &&
+            nextSlide == 0 &&
+            currentSlide == totalItems - 1
+          ) {
+            $(".left-section").slick("slickGoTo", -1);
+          } else if (
+            currentSlide < nextSlide &&
+            currentSlide == 0 &&
+            nextSlide == totalItems - 1
+          ) {
+            $(".left-section").slick("slickGoTo", totalItems);
+          } else {
+            $(".left-section").slick("slickGoTo", totalItems - 1 - nextSlide);
+          }
+
+          switch (nextSlide) {
+            case 0:
+              console.log("to 0!");
+              if (window.innerWidth > 1300) {
+               $(".left-section").css("width", "35vw");
+                $(".profile-photo-card").css("width", "35vw");
+                $("#selfie").css({ "width": "30vw", "height": "30vw", "top": "10vh" });
+              } else {
+             $(".left-section").css("width", "50vw");
+                $(".profile-photo-card").css("width", "50vw");
+                $("#selfie").css({ "width": "45vw", "height": "45vw", "top": "5vh" });
+              }
+              break;
+            case 1:
+              console.log("to 1!");
+              $(".left-section").css("width", "50vw");
+              break;
+            case 2:
+              console.log("to 2!");
+              $(".left-section").css("width", "10vw");
+              break;
+          }
+        })
+        .on("mousewheel", function (event) {
+          event.preventDefault();
+          if (event.deltaX < 0 || event.deltaY > 0) {
+            $(this).slick("slickPrev");
+          } else if (event.deltaX > 0 || event.deltaY < 0) {
+            $(this).slick("slickNext");
+          }
+        });
+
+      $(".left-section")
+        .slick({
+          swipe: false,
+          vertical: true,
+          arrows: false,
+          infinite: true,
+          speed: 400,
+          cssEase: "cubic-bezier(0.7, 0, 0.3, 1)",
+          initialSlide: totalItems - 1,
+
+          touchThreshold: 20,
+          mobileFirst: true,
+        })
+        .on("beforeChange", function (event, slick, currentSlide, nextSlide) {
+          if (
+            currentSlide > nextSlide &&
+            nextSlide == 0 &&
+            currentSlide == totalItems - 1
+          ) {
+            $(".right-section").slick("slickGoTo", -1);
+          } else if (
+            currentSlide < nextSlide &&
+            currentSlide == 0 &&
+            nextSlide == totalItems - 1
+          ) {
+            $(".right-section").slick("slickGoTo", totalItems);
+          } else {
+            $(".right-section").slick("slickGoTo", totalItems - 1 - nextSlide);
+          }
+
+          // switch (nextSlide) {
+          //   case 0:
+          //     if (window.innerWidth > 1300) {
+          //       $(".left-section").css("position: absolute;z-index: 2;top: 0;left: 0;width: 35vw;height: 100vh;overflow: hidden;");
+          //       $(".profile-text").css("width: 65vw !important;left: 35%;position: relative;overflow: hidden;border: none;background-color: #efe2ba;display: flex;justify-content: center;align-content: center;align-items: center;");
+          //       $(".profile-photo-card").css("height: 100vh;width: 35vw;position: relative;overflow: hidden;border: none;background-color: #d79922;border-top-right-radius: 5rem;border-bottom-right-radius: 5rem;");
+          //       $("#selfie").css("border-radius: 3rem;width: 30vw;height: 30vw;position: relative;left: 2.5vw;top: 10vh");
+          //     } else {
+            
+          //       $(".left-section").css("position: absolute;z-index: 2;top: 0;left: 0;width: 50vw;height: 100vh;overflow: hidden;");
+          //       $(".profile-text").css("width: 50vw !important;left: 50%;position: relative;overflow: hidden;border: none;background-color: #efe2ba;display: flex;justify-content: center;align-content: center;align-items: center;");
+          //       $(".profile-photo-card").css("height: 100vh;width: 50vw;position: relative;overflow: hidden;border: none;background-color: #d79922;border-top-right-radius: 5rem;border-bottom-right-radius: 5rem;")
+          //       $("#selfie").css("border-radius: 3rem;width: 45vw;height: 45vw;position: relative;left: 2.5vw;top: 5vh;");
+          //     }
+          //     break;
+          //   case 1:
+          //     $(".left-section").css("width", "50vw");
+          //     break;
+          //   case 2:
+          //     $(".left-section").css("width", "10vw");
+          //     break;
+          // }
+        })
+        .on("mousewheel", function (event) {
+          event.preventDefault();
+          if (event.deltaX < 0 || event.deltaY > 0) {
+            $(this).slick("slickNext");
+          } else if (event.deltaX > 0 || event.deltaY < 0) {
+            $(this).slick("slickPrev");
+          }
+        });
+  }
+
+  toSkills() {
+    $(".right-section").slick("slickGoTo", 1);
+  }
+
+  toProjects() {
+    $(".right-section").slick("slickGoTo", 2);
+  }
+}
