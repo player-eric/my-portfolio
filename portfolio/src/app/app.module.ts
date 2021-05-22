16c552147e3c9e108f4b918d4c45de03922b36b6
Shiqin Yan <digimonyan@gmail.com>
2021-05-21 21:42:55 -0400

    before structuring homepage

--- portfolio/src/app/app.module.ts
+++ portfolio/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TabsComponent } from './tabs/tabs.component';
+import { HomepageComponent } from './homepage/homepage.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TabsComponent
+    TabsComponent,
+    HomepageComponent
   ],
   imports: [
     BrowserModule,
