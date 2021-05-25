30a3e95bb6b820483d97bf3d6827c8269871069c
Shiqin Yan <digimonyan@gmail.com>
2021-05-25 19:12:59 -0400

    updated project list, start implementing dynamic proj detail component

--- src/app/projectList/projects.service.ts
+++ src/app/projectList/projects.service.ts
@@ -6,12 +6,12 @@ import { Project } from './project.model';
 })
 export class ProjectsService {
   private projects: Project[] = [
-    new Project("BrownBytes", "JS, React, Redux", "./assets/selfie.jpeg"),
-    new Project("CoverYourNose", "Python, d3, three.js", "./assets/selfie.jpeg"),
-    new Project("NMT-Visualizer", "Python, d3, three.js", "./assets/selfie.jpeg"),
-    new Project("Animize", "Python, d3, three.js", "./assets/selfie.jpeg"),
-    new Project("MTQE", "Python, d3, three.js", "./assets/selfie.jpeg"),
-    new Project("NEUOS", "Python, d3, three.js", "./assets/selfie.jpeg"),
+    new Project("BrownBytes", "React, Redux, Node.js", "./assets/images/project-logos/brownbytes.png"),
+    new Project("CoverYourNose", "PyTorch, OpenCV, Flask", "./assets/images/project-logos/coveryournose.png"),
+    new Project("NMT-Visualizer", "Fairseq, D3.js, Three.js", "./assets/images/project-logos/nmtvis.png"),
+    new Project("Animize", "TensorFlow, NetworkX", "./assets/images/project-logos/animize.png"),
+    new Project("MTQE", "PyTorch, OpenKiwi", "./assets/images/project-logos/mtqe.png"),
+    new Project("NEUOS", "C++, STL", "./assets/images/project-logos/neuos.png"),
   ]
 
   constructor() { }
